0e1d7e53cf11ceed573d99f5f20da30b5d0ed302
Leonardo Magdarag <143305005+takumoKun@users.noreply.github.com>
2024-04-07 06:33:48 -0700

    Update Form1.vb
    Added the OOP version

--- FileIO/Form1.vb
+++ FileIO/Form1.vb
@@ -1,46 +1,92 @@
 Imports System.IO
 
+Public Class Employee
+    Private Fname, Lname, Gender As String
+    Private ID, Age As Integer
+
+    Public Sub setID(ByVal val As Integer)
+        ID = val
+    End Sub
+
+    Public Sub setFName(ByVal val As String)
+        Fname = val
+    End Sub
+
+    Public Sub setLName(ByVal val As String)
+        Lname = val
+    End Sub
+
+    Public Sub setAge(ByVal val As Integer)
+        Age = val
+    End Sub
+
+    Public Sub setGender(ByVal val As String)
+        Gender = val
+    End Sub
+
+    Public Function getID() As Integer
+        Return ID
+    End Function
+
+    Public Function getFName() As String
+        Return Fname
+    End Function
+
+    Public Function getLName() As String
+        Return Lname
+    End Function
+
+    Public Function getAge() As Integer
+        Return Age
+    End Function
+
+    Public Function getGender() As String
+        Return Gender
+    End Function
+
+
+End Class
+
 Public Class Form1
-    Public Property lineCounter As Integer
-    Public txtData As New List(Of String)
+    Public employee As Employee
+    Public Property employeeCtr As Integer
+    Public employeeList As New List(Of Employee)
 
     'Method when add button is clicked
     Private Sub btn_add_Click(sender As Object, e As EventArgs) Handles btn_add.Click
-        Dim id, firstName, lastName, age, gender As String
-
+        Dim employee As New Employee
         'check if the number of data is zero
-        If txtData.Count - 1 < 0 Then
+        If employeeList.Count - 1 < 0 Then
             'set the id of the item being added as 1
-            id = 1
+            employee.setID(1)
         Else
             'if not 1, then check the id of the last data on the text file
-            Dim lastData As String() = txtData(txtData.Count - 1).Split(","c)
-
+            Dim lastID = employeeList(employeeList.Count - 1).getID
             'set the id to the last id + 1
-            id = lastData(0) + 1
+            employee.setID(lastID + 1)
         End If
 
         Try
             'get the first name using getInput method
-            getInput(firstName, "Enter First Name: ")
+            employee.setFName(getInput("Enter First Name: "))
             'get the last name using getInput method
-            getInput(lastName, "Enter Last Name: ")
+            employee.setLName(getInput("Enter Last Name: "))
             'get the age using getInput method
-            getInput(age, "Enter Age: ")
+            employee.setAge(Integer.Parse(getInput("Enter Age: ")))
             'get gender using getInput method
-            getInput(gender, "Enter Gender: ")
+            employee.setGender(getInput("Enter Gender: "))
 
             'combine the data into a string and add it to the list
-            txtData.Add(id & "," & firstName & "," & lastName & "," & age & "," & gender)
+            employeeList.Add(employee)
 
             'set the line counter to the last index of the list where the added data is added
-            lineCounter = txtData.Count - 1
+            employeeCtr = employeeList.Count - 1
 
             'display the data on the textboxes using the displayData method
-            displayData(lineCounter)
+            displayData()
 
             'enable the buttons if there are more than one data in the list
-            If txtData.Count > 0 Then
+            If employeeList.Count > 0 Then
                 btn_del.Enabled = True
                 btn_edit.Enabled = True
                 btn_search.Enabled = True
@@ -56,8 +102,8 @@ Public Class Form1
     End Sub
 
     'Method to get the input from the user
-    Private Sub getInput(ByRef input As String, ByVal message As String)
-        'ByRef is used to modify the value of the variable passed to the method
+    Private Function getInput(ByVal message As String) As String
+        Dim input As String
         'ByVal is used to pass the value of the variable to the method without modifying it
 
         'use an infinite while loop
@@ -72,12 +118,14 @@ Public Class Form1
                 Exit While
             End If
         End While
-    End Sub
+
+        Return input
+    End Function
 
     'Method when delete button is clicked
     Private Sub btn_del_Click(sender As Object, e As EventArgs) Handles btn_del.Click
         'check if the line counter is within the range of the list
-        If lineCounter >= 0 And lineCounter < txtData.Count Then
+        If employeeCtr >= 0 And employeeCtr < employeeList.Count Then
             'ask user if they are sure
             Dim result As Integer = MessageBox.Show("Are you sure you want to delete this record?", "Delete Record", MessageBoxButtons.YesNo)
 
@@ -87,18 +135,18 @@ Public Class Form1
             End If
 
             'remove the data from the list
-            txtData.RemoveAt(lineCounter)
+            employeeList.RemoveAt(employeeCtr)
             'decrement the line counter
-            lineCounter -= 1
+            employeeCtr -= 1
 
             'if the line counter is less than 0, set it to 0
-            If lineCounter < 0 Then
-                lineCounter = 0
+            If employeeCtr < 0 Then
+                employeeCtr = 0
             End If
         End If
 
         'display the updated data on the textboxes using the displayData method
-        displayData(lineCounter)
+        displayData()
 
         'save the data to the text file using the saveData method
         saveData()
@@ -106,10 +154,10 @@ Public Class Form1
 
     Private Sub btn_prev_Click(sender As Object, e As EventArgs) Handles btn_prev.Click
         'check if the line counter is greater than 0
-        If lineCounter > 0 Then
-            lineCounter -= 1
+        If employeeCtr > 0 Then
+            employeeCtr -= 1
             'display the data on the textboxes using the displayData method
-            displayData(lineCounter)
+            displayData()
         Else
             MessageBox.Show("No more records to show")
         End If
@@ -117,10 +165,10 @@ Public Class Form1
 
     Private Sub btn_next_Click(sender As Object, e As EventArgs) Handles btn_next.Click
         'check if the line counter is less than the number of data in the list
-        If lineCounter < txtData.Count - 1 Then
-            lineCounter += 1
+        If employeeCtr < employeeList.Count - 1 Then
+            employeeCtr += 1
             'display the data on the textboxes using the displayData method
-            displayData(lineCounter)
+            displayData()
         Else
             MessageBox.Show("No more records to show")
         End If
@@ -132,24 +180,22 @@ Public Class Form1
         Dim input As String = InputBox("Enter the number of the field you want to edit: " & vbCrLf & "1. First Name" & vbCrLf & "2. Last Name" & vbCrLf & "3. Age" & vbCrLf & "4. Gender", "Edit Data")
 
         'split the the current data into an array
-        Dim editData = txtData(lineCounter).Split(","c)
+        Dim employee = employeeList(employeeCtr)
 
         'check the input of the user
         Select Case input
             Case "1" ' get the new first name if the input is 1
-                editData(1) = InputBox("Enter the new first Name: ", "Edit Name")
+                employee.setFName(InputBox("Enter the new first Name: ", "Edit Name"))
             Case "2" ' get the new last name if the input is 2
-                editData(2) = InputBox("Enter the new last Name: ", "Edit Name")
+                employee.setLName(InputBox("Enter the new last Name: ", "Edit Name"))
             Case "3" ' get the new age if the input is 3
-                editData(3) = InputBox("Enter the new age : ", "Edit Name")
+                employee.setAge(InputBox("Enter the new age : ", "Edit Name"))
             Case "4" 'get the new gender if the input is 4
-                editData(4) = InputBox("Enter the new gender: ", "Edit Name")
+                employee.setGender(InputBox("Enter the new gender: ", "Edit Name"))
         End Select
 
-        'combine the data into a string and update the data in the list
-        txtData(lineCounter) = String.Join(",", editData)
         'display the updated data on the textboxes using the displayData method
-        displayData(lineCounter)
+        displayData()
         'save the data to the text file using the saveData method
         saveData()
     End Sub
@@ -162,7 +208,14 @@ Public Class Form1
                 'loop through the text file
                 While Not reader.EndOfStream
                     'add the data to the list
-                    txtData.Add(reader.ReadLine())
+                    Dim line As String() = reader.ReadLine().Split(","c)
+                    Dim employee As New Employee
+                    employee.setID(Integer.Parse(line(0)))
+                    employee.setFName(line(1))
+                    employee.setLName(line(2))
+                    employee.setAge(Integer.Parse(line(3)))
+                    employee.setGender(line(4))
+                    employeeList.Add(employee)
                 End While
             End Using
         Catch ex As Exception
@@ -170,8 +223,7 @@ Public Class Form1
         End Try
 
         'display the data on the textboxes using the displayData method
-        displayData(lineCounter)
-
+        displayData()
     End Sub
 
     'Method to save the data to the text file
@@ -179,28 +231,26 @@ Public Class Form1
         'save the data to the text file
         Using writer As New StreamWriter("C:\CodeFolderrrrrrr\visual studio\FileIO\FileIO\data.txt")
             'loop through the list of data
-            For Each line As String In txtData
+            For Each employee As Employee In employeeList
                 'write the data to the text file
-                writer.WriteLine(line)
+                writer.WriteLine(employee.getID & "," & employee.getFName & "," & employee.getLName & "," & employee.getAge & "," & employee.getGender)
             Next
         End Using
     End Sub
 
     Private Sub btn_search_Click(sender As Object, e As EventArgs) Handles btn_search.Click
         'get the search input from the user using the getInput method
-        Dim search As String = InputBox("Enter the ID to search: ")
+        Dim search As Integer = InputBox("Enter the ID to search: ")
         Dim found As Boolean = False
 
         'loop through the list of data
-        For i As Integer = 0 To txtData.Count - 1
-            'split the data into an array
-            Dim usrData As String() = txtData(i).Split(","c)
-            'check if the first element of the array is equal to the search input
-            If usrData(0).ToLower = search.ToLower Then
+        For Each employee As Employee In employeeList
+            'check if the id of current employee is equal to the search id
+            If employee.getID = search Then
                 'display a message if the data is found
                 MessageBox.Show("Data Found!")
-                displayData(i)
-                lineCounter = i
+                employeeCtr = employeeList.IndexOf(employee)
+                displayData()
                 found = True
                 Exit For
             End If
@@ -214,19 +264,18 @@ Public Class Form1
     End Sub
 
     'Method to display the data on the textboxes
-    Private Sub displayData(index As Integer)
-
+    Private Sub displayData()
         'check if the index is within the range of the list
-        If txtData.Count > 0 Then
+        If employeeList.Count > 0 Then
             'split the data into an array
-            Dim usrData As String() = txtData(index).Split(","c)
+            Dim employee As Employee = employeeList(employeeCtr)
 
             'display the data on the textboxes
-            txtb_ID.Text = usrData(0)
-            txtb_fname.Text = usrData(1)
-            txtb_lname.Text = usrData(2)
-            txtb_age.Text = usrData(3)
-            txtb_gender.Text = usrData(4)
+            txtb_ID.Text = employee.getID
+            txtb_fname.Text = employee.getFName
+            txtb_lname.Text = employee.getLName
+            txtb_age.Text = employee.getAge
+            txtb_gender.Text = employee.getGender
         Else
             'if the list is empty, display empty on the textboxes
             txtb_ID.Text = "EMPTY"
